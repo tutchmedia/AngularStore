d77d88c592644c70c1a9f97501785cb5b45f7f03
tutchmedia <cragster33@hotmail.com>
2016-01-08 12:25:03 +0000

    Redeclared username

--- app.js
+++ app.js
@@ -176,8 +176,8 @@ angular.module('app', ['parse', 'ngRoute', 'ui.bootstrap'])
 
     $scope.doLogin = function() {
       //var repoUSER = loginService.create() // this should replace repoUSER = new Parse.User();
-      //var username = $scope.loginForm.username;
-      //var password = $scope.loginForm.password;
+      var username = $scope.loginForm.username;
+      var password = $scope.loginForm.password;
 
 
       console.log($scope.loginForm);
